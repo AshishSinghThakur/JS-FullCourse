5f74494371e99d9a562508b03f228d129ed7aff5
AshishSinghThakur <99022300+AshishSinghThakur@users.noreply.github.com>
2024-03-18 12:49:28 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,16 @@
+const name='Ashish'
+const repoCount=50
+
+//console.log(name + " having repoCount " + repoCount + " Value")
+
+console.log(`My name is ${name} and repoCount is ${repoCount}`);
+
+const gameName=new String('ashishsingh')
+
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+const newSubString=gameName.substring(2,5)
+console.log(newSubString);
