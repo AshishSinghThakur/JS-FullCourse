829617825166e5d3ad59fd172e507727378d7158
AshishSinghThakur <99022300+AshishSinghThakur@users.noreply.github.com>
2024-03-18 13:01:03 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,14 @@
+const score=400
+console.log(score)
+
+const num=new Number(300)
+console.log(num);
+console.log(num.toString());
+console.log(num.toString().length);
+console.log(num);
+console.log(num.toFixed(1));
+const otherNumber=123.8966
+console.log(otherNumber.toPrecision(3));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString());
