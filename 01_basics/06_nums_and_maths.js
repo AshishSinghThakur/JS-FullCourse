d7f1c5f9151b9a817ec9d1640799293d81fcdde4
AshishSinghThakur <99022300+AshishSinghThakur@users.noreply.github.com>
2024-03-19 04:18:46 +0000

    nums and maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,14 +1,27 @@
 const score=400
-console.log(score)
+//console.log(score)
 
 const num=new Number(300)
-console.log(num);
-console.log(num.toString());
-console.log(num.toString().length);
-console.log(num);
-console.log(num.toFixed(1));
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toString().length);
+// console.log(num);
+// console.log(num.toFixed(1));
 const otherNumber=123.8966
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 
 const hundreds=1000000
-console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//*************** Maths *************************
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+// console.log((Math.random()*(max-min+1))+min);
+// console.log(Math.floor(4.5));
+// console.log(Math.ceil(4.3));
