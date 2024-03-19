85a654cb3fc13a9d6a1c6783c430e1b6b79a9695
AshishSinghThakur <99022300+AshishSinghThakur@users.noreply.github.com>
2024-03-19 10:55:36 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,9 @@
+const myArr=[1,2,3,4,5]
+const myArr2=["sss","rrrr","ttttt"]
+const myArr3=new Array(1,2,3)
+// console.log(myArr)
+// console.log(myArr2)
+// console.log(myArr3)
+// console.log(typeof myArr);
+myArr.push(6)
+console.log(myArr)
