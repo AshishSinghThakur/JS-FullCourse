874fe8116d705d9eec34942a5b4179456b62bd9a
AshishSinghThakur <99022300+AshishSinghThakur@users.noreply.github.com>
2024-04-03 11:50:39 +0000

    arrrow 2

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,9 +3,20 @@ const user={
     price: 999,
     welcomeMessage: function (){
         console.log(`${this.username}, welcome to the website`)
+        //console.log(this)
     }
 }
 
-user.welcomeMessage()
-user.username="Allen"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username="Allen"
+// user.welcomeMessage()
+//console.log(this)
+
+// const addTwo=(num1,num2)=>{
+//     return num1 + num2
+// }
+
+//const addTwo=(num1,num2)=> num1 + num2
+const addTwo=(num1,num2)=> (num1 + num2)
+
+console.log(addTwo(3,4))
