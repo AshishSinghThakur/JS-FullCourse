ba3cb05a5ace9f388aeec2f10b7b61f2c0e6d235
AshishSinghThakur <99022300+AshishSinghThakur@users.noreply.github.com>
2024-04-04 07:11:28 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,22 @@
+// if(true){
+//     console.log("executed")
+// }
+
+const isUserLoggedIn= true
+
+// if(isUserLoggedIn){
+//     console.log("User is logged in")
+// }
+
+const num = 400
+// if(num>200){
+//     console.log("number is executed")
+// }
+
+// if(num>300){
+//     const power="fly"
+//     console.log(power)
+// }
+
+const balance = 1000
+// if(balance>500)console.log("balance")
